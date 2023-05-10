5c069d1393b5287a68f0ef31515c7f08a95ea85f
= <=>
2023-05-10 21:03:27 +0300

    Challange ++

---
+++ Artist.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace multitracks.com-dotnet.Entities
+{
+    public class Artist
+    {
+        public int artistID { get; set; }
+        public decimal dataCreation { get; set; }
+        public string title { get; set; }
+        public string biography { get; set; }
+        public string imageURL { get; set; }
+        public string heroURL { get; set; }
+    }
+}
---
+++ Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using Mtdata;
+
+
+public partial class PageToSync_artistDetails : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int artistID = Convert.ToInt32(Request.QueryString["artistID"]);
+        try
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MultiTracksDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {   
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("GetArtistDetails", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter artistIDParam = new SqlParameter("@artistId", artistID);
+                command.Parameters.Add(artistIDParam);
+
+               
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string imageUrl = (string)reader["imageURL"];
+                            string title = (string)reader["title"];
+
+                            titleLabel.Text = title;
+                            imageControl.ImageUrl = imageUrl;
+                        }
+                    }
+                    else
+                    {
+                        // No rows returned
+                        // Display error message or handle the situation as needed
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            string error = "Bağlantı hatası: " + ex.Message;
+        }
+    }
+}
